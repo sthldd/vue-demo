d48a6bedce4b5e465dae2f3aab806a4a6b56184e
mlx <1209624528@qq.com>
2018-08-06 21:51:40 +0800

    input

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# 轱辘 - 一个 Vue UI 组件
+
+[![Build Status](https://www.travis-ci.org/sthldd/vue-demo.svg?branch=master)](https://www.travis-ci.org/sthldd/vue-demo)
+
 ## 介绍
 
 这是我在学习 Vue 过程中做的一个 UI 框架，希望对你有用。
--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-import Vue from 'vue'
-import Button from  './button'
-import Icon from './icon'
-import ButtonGroup from  './button-group'
+import Vue from 'vue';
+import Button from './button';
+import Icon from './icon';
+import ButtonGroup from './button-group';
+import Input from './input';
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
-Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
+Vue.component('g-input', Input);
 new Vue({
-    el:'#app',
-    data:{
-        loading1:false,
-        loading2:true,
-        loading3:false,
-    }
-})
+  el: '#app',
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false
+  }
+});
