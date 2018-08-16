e556fddb4536c68499155ac1cd4cdb08bd96e99f
mlx <1209624528@qq.com>
2018-08-16 23:32:45 +0800

    24

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button';
 import Icon from './icon';
 import ButtonGroup from './button-group';
 import Input from './input';
+import Row from './row';
+import Col from './col';
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
 Vue.component('g-input', Input);
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
 
 new Vue({
   el: '#app',
@@ -15,11 +19,11 @@ new Vue({
     loading1: false,
     loading2: true,
     loading3: false,
-    message:'hi'
+    message: 'hi'
   },
-  methods:{
-    inputChange(e,){
-      console.log(e.target.value)
+  methods: {
+    inputChange(e) {
+      console.log(e.target.value);
     }
   }
 });
