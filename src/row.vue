b473415062946b947825adc1bf68df7ef126c485
mlx <1209624528@qq.com>
2018-08-20 23:13:18 +0800

    栅栏布局

--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+<script lang="ts">
+  export default{
+    name:'GuluRow',
+    props:{
+      gutter:{
+        type:[Number,String]
+      }
+    },
+    mounted(){
+      this.$children.forEach((vm)=>{
+        console.log(this)
+        console.log(vm)
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
 .row{
