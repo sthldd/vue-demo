b92a61fc7f6e574568537a231a07189706f4619d
mlx <1209624528@qq.com>
2018-08-21 21:45:08 +0800

    精简

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -11,10 +11,15 @@
         type:[Number,String]
       }
     },
+    computed:{
+      rowStyle(){
+        let {gutter} = this
+        return {marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}
+
+      }
+    },
     mounted(){
       this.$children.forEach((vm)=>{
-        console.log(this)
-        console.log(vm)
         vm.gutter = this.gutter
       })
     }
