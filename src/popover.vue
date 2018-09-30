e47fac01b336c089eae2d240c6d11e401c2e7bec
mlx <1209624528@qq.com>
2018-09-30 14:26:11 +0800

    popover style

--- src/popover.vue
+++ src/popover.vue
@@ -1,54 +1,93 @@
 <template>
-  <div class="popover" @click.stop="onClick"> <!-- 禁止冒泡到document，否则会隐藏两次 -->
-    <div ref="contentWrapper" class="content-wrapper"  v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="trigger">
-      <slot></slot>
-    </span>
-  </div>
-</template>
+    <span ref="trigger" style="display:inline-block">
+       <slot></slot>
+     </span>
+   </div>
+ </template>
 <script>
 export default {
   name:'GuluPopover',
   data(){
     return {visible:false}
   },
-  mounted(){
-    console.log(this.$refs.trigger)
-  },
   methods:{
-    onClick(){
-      this.visible = !this.visible
-      if(this.visible === true){
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper)
-          let {width,height,top,left} = this.$refs.trigger.getBoundingClientRect()
-          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-          this.$refs.contentWrapper.style.left = left + window.scrollX +'px'
-          let eventHandler = ()=>{
-            this.visible = false
-            document.removeEventListener('click',eventHandler)
+    positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.trigger.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+    onClickDocument (e) {
+      if (this.$refs.popover &&(this.$refs.popover === e.target || this.$refs.popover.contains(e.target))){ return }
+      if (this.$refs.contentWrapper &&(this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))){ return }
+      this.close()
+      },
+    open () {
+      this.visible = true
+      this.$nextTick(() => {
+        this.positionContent()
+        document.addEventListener('click', this.onClickDocument)
+      })
+    },
+    close () {
+      this.visible = false
+      document.removeEventListener('click', this.onClickDocument)
+    },
+    onClick (event) {
+        if (this.$refs.trigger.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
           }
-          document.addEventListener('click',eventHandler)
-        })
-      }
+        }
+       }
     }
   }
-}
 </script>
 <style lang="scss" scoped>
+  $border-color:#333;
+  $border-radius:4px;
   .popover{
     display: inline-block;
     vertical-align: top;
     position: relative;
-    border:1px solid green;
+
   }
   .content-wrapper{
     position: absolute;
     transform: translateY(-100%);
-    border:1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5)
+    border:1px solid $border-color;
+    border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5)); //倒三角没有阴影
+    background: white;
+    padding:0.5em 1em;
+    margin-top:-10px;
+    max-width: 20em;
+    word-break: break-all;
+    &::before,&::after{
+      content: '';
+      display: block;
+      height:0px;
+      width:0px;
+      border:10px solid transparent;
+      position: absolute;
+      left:10px;
+      top:100%;
+      border-top-color: black;
+    }
+    &::after{
+      top:calc(100% - 1px);
+      border-top-color: white;
+    }
+    &::before{
+      top:100%;
+      border-top-color: black;
+    }
   }
 </style>
 
